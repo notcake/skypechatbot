14f17b428b1a791b90c6520cfcbe2eed873c7608
_cake <cakenotfound@googlemail.com>
2013-07-25 20:10:27 +0100

    Fix to GenericUrlSpanHandler regular expressions.

--- MessageSpanHandlers/GenericUrlSpanHandler.cs
+++ MessageSpanHandlers/GenericUrlSpanHandler.cs
@@ -16,17 +16,17 @@ namespace ChatBot.MessageSpanHandlers
     {
         private string[] Urls = new string[]
         {
-            "https?://(www\\.)?xkcd\\.com(/\\d)*",
-            "https?://(www\\.)?facepunch.com/showthread.php\\?(.)*(t=\\d{7})",
+            "https?://(www\\.)?xkcd\\.com(/\\d+)?/?",
+            "https?://(www\\.)?facepunch.com/showthread.php\\?.*t=(\\d{7})",
 			
 			// Steam
-			"https?://(www\\.)?store.steampowered.com/app/(\\d)+",
-			"https?://(www\\.)?steamcommunity.com/sharedfiles/filedetails(.)+",
-			"https?://(www\\.)?steamcommunity.com/app/(\\d)+([a-zA-Z0-9/]*)",
+			"https?://(www\\.)?store.steampowered.com/app/(\\d+)",
+			"https?://(www\\.)?steamcommunity.com/sharedfiles/filedetails.+",
+			"https?://(www\\.)?steamcommunity.com/app/(\\d+)([a-zA-Z0-9/]*)",
 			"https?://(www\\.)?steamcommunity.com/id/([a-zA-Z0-9_]+)",
-			"https?://(www\\.)?steamcommunity.com/profiles/(\\d)+",
+			"https?://(www\\.)?steamcommunity.com/profiles/(\\d+)",
 			"https?://(www\\.)?steamcommunity.com/groups/([a-zA-Z0-9_]+)",
-			"https?://(www\\.)?steamcommunity.com/gid/(\\d)+",
+			"https?://(www\\.)?steamcommunity.com/gid/(\\d+)",
 			"https?://(www\\.)?steamcommunity.com/discussions/forum/([0-9/]+)"
         };
 
