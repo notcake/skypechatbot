8c347981de4ca75c9d5a3453430bdbfe791d2775
PotcFdk <PotcFdk@gmx.net>
2013-08-04 18:16:18 +0200

    Added EvalCommandHandler (inactive). I have no idea how I'm going to try this out.

---
+++ CommandHandlers/EvalCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatBot.CommandHandlers
+{
+    public class EvalCommandHandler : ICommandHandler
+    {
+       public static double Evaluate(string expression)  
+       {  
+           System.Data.DataTable table = new System.Data.DataTable();  
+           table.Columns.Add("expression", string.Empty.GetType(), expression);  
+           System.Data.DataRow row = table.NewRow();  
+           table.Rows.Add(row);  
+           return double.Parse((string)row["expression"]);  
+       }
+
+        public string Command
+        {
+            get { return "eval"; }
+        }
+
+        public void Handle(MessageSink messageSink, Command command)
+        {
+            messageSink(Evaluate(command.FullArguments));
+        }
+    }
+}
