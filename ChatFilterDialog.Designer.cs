9c16f50fa915bad2887e07b62b63d2d671da165b
_cake <cakenotfound@googlemail.com>
2013-09-06 12:18:20 +0100

    ChatFilterDialog.

--- ChatFilterDialog.Designer.cs
+++ ChatFilterDialog.Designer.cs
@@ -39,6 +39,7 @@
             this.ChatListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.ChatListBox.CheckOnClick = true;
             this.ChatListBox.FormattingEnabled = true;
             this.ChatListBox.HorizontalScrollbar = true;
             this.ChatListBox.IntegralHeight = false;
