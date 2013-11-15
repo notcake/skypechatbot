23d01cfbe1d44ff59d7ae7152c34d11ec88c66b2
PotcFdk <PotcFdk@gmx.net>
2013-11-15 23:08:41 +0100

    Added !slogan command.

---
+++ CommandHandlers/SloganCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Eka.Web.SloganGenerator;
+
+namespace ChatBot.CommandHandlers
+{
+    public class SloganCommandHandler : ICommandHandler
+    {
+        public string Command
+        {
+            get { return "slogan"; }
+        }
+
+        public void Handle(MessageSink messageSink, Command command)
+        {
+            Slogan slogan = new Slogan(command.FullArguments);
+            if (slogan.Success)
+                messageSink(slogan.SloganText);
+        }
+    }
+}
