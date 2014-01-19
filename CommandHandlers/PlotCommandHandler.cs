502893232dece5883767d2112644f89eee23414e
PotcFdk <PotcFdk@gmx.net>
2014-01-19 21:01:45 +0100

    Added PlotCommandHandler.

---
+++ CommandHandlers/PlotCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Eka.Web.PlotGenerator;
+
+namespace ChatBot.CommandHandlers
+{
+    public class PlotCommandHandler : ICommandHandler
+    {
+        public string Command
+        {
+            get { return "plot"; }
+        }
+
+        public void Handle(MessageSink messageSink, Command command)
+        {
+            Plot plot = new Plot();
+            if (plot.Success)
+                messageSink(plot.PlotText);
+        }
+    }
+}
