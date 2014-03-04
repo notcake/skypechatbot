a3c0b517f3bff007330b6bcfd9b642c6158952f2
PotcFdk <PotcFdk@gmx.net>
2014-03-04 21:09:37 +0100

    Added DefinitionCommandHandler

---
+++ CommandHandlers/DefinitionCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Eka.Web.UrbanDictionary;
+
+namespace ChatBot.CommandHandlers
+{
+    public class DefinitionCommandHandler : ICommandHandler
+    {
+        public string Command
+        {
+            get { return "define"; }
+        }
+
+        public void Handle(MessageSink messageSink, Command command)
+        {
+            UrbanDictionary dictionary = new UrbanDictionary(command.FullArguments);
+            if (dictionary.Success)
+                messageSink(dictionary.Definition + (dictionary.Example != null ? "\nExample: " + dictionary.Example : ""));
+        }
+    }
+}
