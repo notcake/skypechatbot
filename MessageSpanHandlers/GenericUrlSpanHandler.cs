c3a5749c30a5347a0635ae92437b753c02959066
PotcFdk <PotcFdk@gmx.net>
2014-03-06 22:06:11 +0100

    Improved steam discussion link precision.

--- MessageSpanHandlers/GenericUrlSpanHandler.cs
+++ MessageSpanHandlers/GenericUrlSpanHandler.cs
@@ -29,12 +29,13 @@ namespace ChatBot.MessageSpanHandlers
             "https?://(www\\.)?store\\.steampowered\\.com/app/(\\d+)",
             "https?://(www\\.)?forums\\.steampowered\\.com/forums/showthread\\.php\\?t=(\\d{7})",
             "https?://(www\\.)?steamcommunity\\.com/sharedfiles/filedetails.+",
-            "https?://(www\\.)?steamcommunity\\.com/app/(\\d+)([a-zA-Z0-9/_-]*)",
             "https?://(www\\.)?steamcommunity\\.com/id/([a-zA-Z0-9_-]+)",
             "https?://(www\\.)?steamcommunity\\.com/profiles/(\\d+)",
-            "https?://(www\\.)?steamcommunity\\.com/groups/([a-zA-Z0-9_-]+)",
             "https?://(www\\.)?steamcommunity\\.com/gid/(\\d+)",
             "https?://(www\\.)?steamcommunity\\.com/discussions/forum/([0-9/]+)"
+            "https?://(www\\.)?steamcommunity\\.com/(app|groups|workshop/filedetails)/discussions?/(\\d+)/(\\d+)"
+            "https?://(www\\.)?steamcommunity\\.com/app/(\\d+)([a-zA-Z0-9/_-]*)",
+            "https?://(www\\.)?steamcommunity\\.com/groups/([a-zA-Z0-9_-]+)",
         };
 
         public void IdentifyActionSpans(ActionSpanSink actionSpanSink, string message)
