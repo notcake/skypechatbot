650dd50deb118aa66586b2d07410dda46fa53a8f
Techbot <techbot121@gmail.com>
2015-05-11 02:36:14 +0200

    added VineUrlHandler

---
+++ MessageSpanHandlers/VineUrlSpanHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using Eka.Web.Vine;
+
+namespace ChatBot.MessageSpanHandlers
+{
+    public class VineUrlSpanHandler : IMessageSpanHandler
+    {
+        private readonly string[] VineUrls =
+        {
+            "https?://(www\\.)?vine\\.co/v/([a-zA-Z0-9_]+)"
+        };
+
+        public void IdentifyActionSpans(ActionSpanSink actionSpanSink, string message)
+        {
+            foreach (var vineUrlPattern in VineUrls)
+            {
+                foreach (Match match in new Regex(vineUrlPattern, RegexOptions.IgnoreCase).Matches(message))
+                {
+                    actionSpanSink(match, null);
+                }
+            }
+        }
+
+        public void HandleSpan(MessageSink messageSink, MessageActionSpan actionSpan)
+        {
+            var vineid = actionSpan.Match.Groups[2].ToString();
+
+            var vine = new Vine(vineid);
+
+            messageSink("Vine: " + vine.Title + " By: " + vine.Author);
+        }
+    }
+}
